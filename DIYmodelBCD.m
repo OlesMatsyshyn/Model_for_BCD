7176eca39a19cadab4af352f94dfce0eb061369d
Oles <oles.matsyshyn@gmail.com>
2024-07-08 16:18:46 +0800

    Strained DIY model implemented

--- DIYmodelBCD.m
+++ DIYmodelBCD.m
@@ -1,13 +1,13 @@
 % System initialization
 %% Define the strained hoppings and the parameters of the Hamiltonian
 t1x = 1;       % In the eV units
-t1y = 0.8;       % In the eV units
+t1y = 1;       % In the eV units
 t2  = 0.;     % In the eV units
 alpha_g = 0.3;% In the eV units
 mu = -1;      % chemical potential
 U = 0.75;     % Cooper pairs coupling strength. Yanase has 1.5 of gamma_0 for graphene.                       
 
-lE = 0.1;
+lE = 0.1; % to split the bands
 
 % U      |      Tc
 % --------------------
@@ -25,9 +25,8 @@ fFD  = @(x) 1/2-tanh(0.5*(x)/Temp)/2; % Fermi-Dirac function
 dfFD = @(x) -(0.25).*sech(0.5*(x)/Temp).^2; % We should later divide by Temp
 
 % Grid density
-nx = 2609;
-ny = 2600;
-q = 0.6;
+nx = 609;
+ny = 600;
 % The f of the strained graphene
 xik  = @(kx,ky) -2*(t1x*cos(kx)+t1y*cos(ky))+4*t2*cos(kx).*cos(ky)-mu;
 gkx  = @(kx,ky) -alpha_g*sin(ky);
@@ -72,23 +71,45 @@ dDH_BdG= [zeros(2,2), 1j*sy ;
 nbands = length(dDH_BdG);
 nebands = fix(nbands/2);
 
+d1 = [1, 0];
+d2 = [0, 1];
+
 a1 = [2*pi, 0];
 a2 = [0, 2*pi];
+
+%% The strain
+th = pi/20;
+epsilon = 0.3;
+sigma = 0.165;
+ep = epsilon*[cos(th)^2 - sigma*sin(th)^2,(1+sigma)*cos(th)*sin(th)   ;
+              (1+sigma)*cos(th)*sin(th)  ,sin(th)^2 - sigma*cos(th)^2];
+% Stain of the space vectors
+SR = eye(2) + ep;
+d1_strained = (SR*d1')';
+d2_strained = (SR*d2')';
+% Stain of the recirocal vectors
+a1_strained = (SR\(a1'))'; % Equivalent to inv(SR)*(a1_star'), but the current vertion is faster
+a2_strained = (SR\(a2'))';
 
 v_table = zeros (nebands, nebands, 2); 
 energy  = zeros (nx+1, ny+1, nebands);
 BCD     = zeros(2,2,2);
 BCD2    = zeros(2,2,2);
 BCDD    = zeros(nx+1,ny+1,2,2,2);
+BCDD2    = zeros(nx+1,ny+1,2,2,2);
 for i = 0 : nx
     for j = 0 : ny
-        k = [-pi,-pi] + (i/nx) * a1 + (j/ny) * a2;
-        [Vec,en] = eigenshuffle(H_N(k(1),k(2)));
+        k = [-pi,-pi] + (i/nx) * a1_strained + (j/ny) * a2_strained;
+        ki = d1_strained(1)*k(1) + d1_strained(2)*k(2);
+        kj = d2_strained(1)*k(1) + d2_strained(2)*k(2);
+        [Vec,en] = eigenshuffle(H_N(ki,kj));
+        % test that energy(i,j+1,:) = energy(nx+1,j+1,:)?
+
         energy(i+1,j+1,:) = en(:); % band energy at k
         for n = 1 : nebands
             for m = 1 : nebands
-                v_table(n,m,1) = Vec(:,n)'*dxH_N(k(1),k(2))*Vec(:,m);
-                v_table(n,m,2) = Vec(:,n)'*dyH_N(k(1),k(2))*Vec(:,m);
+                v_table(n,m,1) = Vec(:,n)'*dxH_N(ki,kj)*Vec(:,m);
+                v_table(n,m,2) = Vec(:,n)'*dyH_N(ki,kj)*Vec(:,m);
             end
         end
         for n = 1 : nebands
@@ -108,6 +129,9 @@ for i = 0 : nx
                                 (v_table(n,m,beta)*v_table(m,n,gamma)-v_table(m,n,beta)*v_table(n,m,gamma))/(en(n)-en(m))^2;
 
                             BCDD(i+1,j+1,alpha,beta,gamma) = BCDD(i+1,j+1,alpha,beta,gamma)+...
+                                + (v_table(n,n,alpha)*dfFD(en(n))-v_table(m,m,alpha)*dfFD(en(m)))*...
+                                v_table(n,m,beta)*v_table(m,n,gamma)/(en(n)-en(m))^2;
+                            BCDD2(i+1,j+1,alpha,beta,gamma) = BCDD2(i+1,j+1,alpha,beta,gamma)+...
                                 + v_table(n,n,alpha)*dfFD(en(n))*...
                                 (v_table(n,m,beta)*v_table(m,n,gamma)-v_table(m,n,beta)*v_table(n,m,gamma))/(en(n)-en(m))^2;
 
@@ -118,97 +142,52 @@ for i = 0 : nx
         end
     end
 end
-dkxdky = 1/(nx*ny); % dkx dky / (2*pi)^2
+dkxdky = abs(a1_strained(1)*a2_strained(2)-a1_strained(2)*a2_strained(1))/((2*pi)^2*nx*ny); % dkx dky / (2*pi)^2
 BCD = BCD * dkxdky/Temp;
+BCD2 = BCD2 * dkxdky/Temp;
 figure
-surf(energy(:,:,1))
+surf(energy(:,:,1),'edgecolor','none')
 hold on
-surf(energy(:,:,2))
+surf(energy(:,:,2),'edgecolor','none')
 figure
 hold on
 plot(energy(fix(end/2),:,1))
 plot(energy(fix(end/2),:,2))
-% Dq = qx*0+0.1; % seed value
-% Jq = qx*0;
-% nqx = 30;
-% qx = linspace(-0.1,0.1,nqx);
-%%  Not yet required 
-% tic
-% cprintf('hyper','Starting the canlculation... Hold on... It"s not so simple...\n')% XX component (just a nice dislay of the statement)
-% parfor iqx = 1:nqx
-%     iter = 0;             % number of iterations from this q completed
-%     discr = 1;            % just to inititalize while
-%     % For the current q find the Bogolubov Band structure:
-%     band   = zeros (nbands, nbands, nx+1, ny+1); 
-%     energy = zeros (nbands, nx+1, ny+1);
-%     while discr > 0.01 && ~isnan(discr) && abs(Dq(iqx)) > 10^(-6) % convergence cryterium
-%         iter = iter + 1;  % update iteration index
-%         % Do not count the boundary twise! start from 0 and end at nx-1,
-%         % or from 1 and end at nx
-%         for i = 0 : nx
-%             for j = 0 : ny
-%                 k = [-pi,-pi] + (i/nx) * a1 + (j/ny) * a2;
-%                 [Vec,Val] = eigenshuffle(H_BdG(k(1),k(2),qx(iqx),Dq(iqx)));
-%                 band(:,:,i+1,j+1) = Vec;       % column eigenvectors at which kx, at which ky, ;
-%                                            % eigenvector vi = band(:,i,kx,ky);
-%                 energy(:,i+1,j+1) = Val(:);    % band, at which kx, at which ky;
-%             end
-%         end
-% 
-%         Dnm  = @(n,m,ikx,jky) band(:,n,ikx,jky)'*dDH_BdG*band(:,m,ikx,jky);
-%                
-%         Dmem = Dq(iqx);                 % memorise the D before the update
-% 
-%         IntegralN = zeros(1,nbands);    % split the task between cores
-%         for n = 1 : nbands
-%             for i = 0 : nx
-%                 for j  = 0 : ny   
-%                     IntegralN(n) = IntegralN(n) + fFD(energy(n,i+1,j+1))*Dnm(n,n,i+1,j+1);
-%                 end
-%             end
-%         end
-%         % Updating the gap
-%         Dq(iqx) = - 0.5 * U * dkxdky * sum(IntegralN);
-%         discr = abs((Dq(iqx)-Dmem)/Dmem);
-%         %disp(['iteration = ', num2str(iter), ' finished, Delta = ', num2str(fix(Dq(iqx)*1000)/1000),...
-%         %                                        ', discrepancy = ',num2str(fix(discr*10^6)/10^6)]);      
-%     end
-%     if abs(Dq(iqx)) < 10^(-6)
-%         Dq(iqx) = 0;
-%     end
-% %     for i = 0 : nx
-% %         for j = 0 : ny
-% %             k = [-pi,-pi] + (i/nx) * a1 + (j/ny) * a2;
-% %             [Vec,Val] = eigenshuffle(H_BdG(k(1),k(2),qx(iqx),Dq(iqx)));
-% %             band(:,:,i+1,j+1) = Vec;       % column eigenvectors at which kx, at which ky, ;
-% %                                        % eigenvector vi = band(:,i,kx,ky);
-% %             energy(:,i+1,j+1) = Val(:);    % band, at which kx, at which ky;
-% %         end
-% %     end
-% %     % For a converged iterative process compute the current
-% %     % Note, the Hamiltonian is k dependent, and the vectors are stored by
-% %     % indeces, that is why we need so many entries
-% %     Jnm  = @(n,m,ikx,jky,kx,ky,q) band(:,n,ikx,jky)'*dxH_BdG(kx,ky,q)*band(:,m,ikx,jky);
-% %     for n = 1 : nbands
-% %         for i = 0 : nx
-% %             for j  = 0 : ny
-% %                 k = [-pi,-pi] + (i/nx) * a1 + (j/ny) * a2;
-% %                 Jq(iqx) = Jq(iqx) + fFD(energy(n,i+1,j+1))*Jnm(n,n,i+1,j+1,k(1),k(2),qx(iqx));
-% %             end
-% %         end
-% %     end
-%     % displaying progress
-%     % progress = fix(100 * iqx / nqx);
-%     % dispstat([sprintf('%d%% completed, time passed: ',progress) num2str(fix(10*t)/10)...
-%     %            ' sec, time left (approx): ' num2str(fix(10*t*(nqx-iqx)/iqx)/10) ' sec'],progress);
-%     disp(['I finished ', num2str(iqx)])
-% end
-% Jq = Jq * dkxdky;
-% plot(qx,Dq)
-% xlabel('q_x')
-% ylabel('\Delta_q')
-% 
-% figure
-% plot(qx,real(Jq))
-% xlabel('q_x')
-% ylabel('J_q')
+disp(['BCD estimation is: ', num2str(sum(abs(BCD),'all')),'; if more than 10e-10, its finite'])
+
+tic
+iter = 0;             % number of iterations from this q completed
+%% For the current q find the Bogolubov Band structure:
+band   = zeros (nbands, nbands, nx+1, ny+1); 
+energy = zeros (nbands, nx+1, ny+1);
+Dnn    = zeros (nbands, nx+1, ny+1);
+Dq = 0.1; % seed value
+discr = 1;
+while discr > 0.01 && ~isnan(discr) && abs(Dq) > 10^(-6) % convergence cryterium
+    iter = iter + 1;  % update iteration index
+    % or from 1 and end at nx
+    while discr > 10^(-6) && abs(Dq) > 10^(-5) % convergence cryterium
+        for i = 0 : nx
+            for j = 0 : ny
+                k = [-pi,-pi] + (i/nx) * a1_strained + (j/ny) * a2_strained;
+                ki = d1_strained(1)*k(1) + d1_strained(2)*k(2);
+                kj = d2_strained(1)*k(1) + d2_strained(2)*k(2);
+                [Vec,Val] = eigenshuffle(H_BdG(ki,kj,0,Dq));
+                energy(:,i+1,j+1) = Val(:);    % band, at which kx, at which ky;
+                for n = 1 : nbands
+                    Dnn(n,i+1,j+1)  = Vec(:,n)'*dDH_BdG*Vec(:,n);
+                end
+            end
+        end     
+        Dmem = Dq;                 % memorise the D before the update
+        % Updating the gap
+        Dq = - 0.5 * U * dkxdky * sum(sum(sum(fFD(energy).*Dnn)));
+        discr = abs((Dq-Dmem));
+        %disp(['iteration = ', num2str(iter), ' finished, Delta = ', num2str(fix(Dq(iqx)*1000)/1000),...
+        %                                        ', discrepancy = ',num2str(fix(discr*10^6)/10^6)]);      
+    end     
+end
+if abs(Dq) < 10^(-6)
+    Dq = 0;
+end
+disp(['Del estimation is: ', num2str(Dq),'; if more than 10e-4, its finite, also should be less then 1'])
